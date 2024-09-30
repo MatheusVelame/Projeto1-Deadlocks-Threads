10e11ffb94e66dea77c51f456882cd9b969ca13a
Matheus Velame <142419976+MatheusVelame@users.noreply.github.com>
2024-09-30 04:32:13 +0000

    Concluindo a função do produtor

--- produto_consumidor_variaveis_de_condicoes.c
+++ produto_consumidor_variaveis_de_condicoes.c
@@ -5,7 +5,32 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_produtor = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_consumidor = PTHREAD_COND_INITIALIZER;
 
+int buffer[5];
+int cont = 0;
+
 void produtor(void* arg){
+
+    int produto;
+
+    while(1){
+
+        produto++;
+
+        pthread_mutex_lock(&mutex);
+
+        while(cont == 5){
+            printf("Buffer cheio. Produtor esperando!\n");
+            pthread_cond_wait(&cond_produtor, &mutex);
+        }
+
+        buffer[cont] = produto;
+        cont++;
+        printf("Produtor adicionou um item ao buffer! Produtos no buffer: %d\n", cont);
+
+        pthread_cond_signal(&cond_consumidor);
+        pthread_mutex_unlock(&mutex);
+
+    }
 
 }
 
