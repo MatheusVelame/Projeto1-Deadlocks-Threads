640df692a734b1a9e1d4317853889a6dfd259908
Matheus Velame <142419976+MatheusVelame@users.noreply.github.com>
2024-10-04 13:15:50 +0000

    ajuste

--- leitores_escritores_rwlocks.c
+++ leitores_escritores_rwlocks.c
@@ -1,51 +1,61 @@
-#include <pthread.h> // biblioteca para criar e gerencia threads
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define NUM_leitorS 5
+#define NUM_escritorS 2
 
 pthread_rwlock_t rwlock;
-int recurso_compartilhado = 0;
-int escritores_espera = 0;
+int shared_data = 0;
 
-void* leitor(void* arg){
+void* leitor(void* id) {
+    int leitor_id = *((int*)id);
     while (1) {
-        if (escritores_espera > 0) {
-            continue; // desistência temporária
-        }
-        pthread_rwlock_rdlock(&rwlock);  // lock de leitura
-        printf("Leitor %ld - leitura do valor %d\n", (long)arg, recurso_compartilhado);
-        pthread_rwlock_unlock(&rwlock);  // lock - liberar
-        break;
+        pthread_rwlock_rdlock(&rwlock); // Adquire o lock para leitura
+        printf("Leitor %d: valor do dado compartilhado é %d\n", leitor_id, shared_data);
+        pthread_rwlock_unlock(&rwlock); // Libera o lock
+        sleep(rand() % 3 + 1); // Simula tempo de processamento
     }
     return NULL;
 }
 
-void* escritor(void* arg) {
-    escritores_espera++;  // escritor - espera
-
-    pthread_rwlock_wrlock(&rwlock);  // lock - adquirir
-    recurso_compartilhado += 1;
-    printf("Escritor %ld - atualizou valor para %d\n", (long)arg, recurso_compartilhado);
-    pthread_rwlock_unlock(&rwlock);  // lock - liberar
-
-    escritores_espera--;  // escritor - indica término escrita
+void* escritor(void* id) {
+    int escritor_id = *((int*)id);
+    while (1) {
+        pthread_rwlock_wrlock(&rwlock); // Adquire o lock para escrita (prioridade sobre leitores)
+        shared_data++;
+        printf("Escritor %d: atualizou o valor do dado compartilhado para %d\n", escritor_id, shared_data);
+        pthread_rwlock_unlock(&rwlock); // Libera o lock
+        sleep(rand() % 3 + 1); // Simula tempo de processamento
+    }
     return NULL;
 }
 
 int main() {
-    pthread_t t_leitor1, t_leitor2, t_escritor;
+    pthread_t leitors[NUM_leitorS], escritors[NUM_escritorS];
+    int leitor_ids[NUM_leitorS], escritor_ids[NUM_escritorS];
 
-    pthread_rwlock_init(&rwlock, NULL);  // lock - inicializar
+    pthread_rwlock_init(&rwlock, NULL);
 
-    // threads - leitores e escriteras
-    pthread_create(&t_leitor1, NULL, leitor, (void*)1);
-    pthread_create(&t_escritor, NULL, escritor, (void*)1);
-    pthread_create(&t_leitor2, NULL, leitor, (void*)2);
+    for (int i = 0; i < NUM_leitorS; i++) {
+        leitor_ids[i] = i + 1;
+        pthread_create(&leitors[i], NULL, leitor, &leitor_ids[i]);
+    }
 
-    // threads - aguardar conclusão
-    pthread_join(t_leitor1, NULL);
-    pthread_join(t_escritor, NULL);
-    pthread_join(t_leitor2, NULL);
+    for (int i = 0; i < NUM_escritorS; i++) {
+        escritor_ids[i] = i + 1;
+        pthread_create(&escritors[i], NULL, escritor, &escritor_ids[i]);
+    }
 
-    pthread_rwlock_destroy(&rwlock);  // lock - destruir
+    for (int i = 0; i < NUM_leitorS; i++) {
+        pthread_join(leitors[i], NULL);
+    }
+    for (int i = 0; i < NUM_escritorS; i++) {
+        pthread_join(escritors[i], NULL);
+    }
+
+    pthread_rwlock_destroy(&rwlock); // Destrói o rwlock
 
     return 0;
 }
